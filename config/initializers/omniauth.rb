512f6b9007eb419725d93b7aa0e0331845646c74
Annie <anniehckim@gmail.com>
2016-09-21 15:36:20 -0400

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '624452694424064', '8be697f91446c907e52f7b36c3657031'
+  provider :facebook, 'APP_ID', 'APP_SECRET'
 end
